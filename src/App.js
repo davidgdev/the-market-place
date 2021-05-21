224f7f7296b00e1f44edfd780892e4d4ed598fcd
Denisse Rivas <iqrivas@gmail.com>
2021-05-20 21:29:20 -0500

    Testing Deploy Preview

--- src/App.js
+++ src/App.js
@@ -20,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header> */}
+      <h1>PRUEBA DEL PULL REQUEST DEPLOY</h1>
       <HorizontalCard title="blue notebook" description="Save all your notes in this amazing notebook. 
                 It has 100 blank pages to be filled with your stories." price="10.00" quantity="5"/>
     </div>
