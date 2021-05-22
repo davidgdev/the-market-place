870fd02584a8afde02928dd5a7767af08ffb4dee
Denisse Rivas <iqrivas@gmail.com>
2021-05-22 18:30:12 -0500

    Adds Product Details

--- src/components/ProductDetails.jsx
+++ src/components/ProductDetails.jsx
@@ -1,30 +1,24 @@
 import React from 'react';
-import '../sass/components/_ProductDescription.scss';
+import '../sass/components/_ProductDetails.scss';
 
-import Photo from './Photo';
-import PhotoReel from './PhotoReel';
 import ProductButton from './ProductButton';
 
 
-function ProductDescription ({title, description, price, quantity}) {
+function ProductDescription ({title, description, price, quantity, seller}) {
     return (
-     <section className="prodDesc">
-        <PhotoReel photos=""/>
-        <Photo size="photo__img photo__img--large"/>
-        <div className="prodDesc__info">
-            <p className="prodDesc__info-title">blue notebook</p>
-            <p className="prodDesc__info-description">Save all your notes in this amazing notebook. It has 100 blank pages to be filled with your stories.</p>
-            <div className="prodDesc__info-bottom">
-                <div className="prodDesc__info-properties">
-                    <p><span className="prodDesc__details-property">Seller:<br/></span> Fulanito Detal</p>
-                    <p><span className="prodDesc__details-property">Available:<br/></span> 83 units</p>
+     <section className="prodDetails">
+            <p className="prodDetails__title">{title}</p>
+            <p className="prodDetails__description">{description}</p>
+            <div className="prodDetails__bottom">
+                <div className="prodDetails__properties">
+                    <p><span className="prodDetails__property">Seller:<br/></span> {seller}</p>
+                    <p><span className="prodDetails__property">Available:<br/></span> {quantity} units</p>
                 </div>
-                <div className="prodDesc__info-buy">
-                    <p><span className="prodDesc__details-property">Price:</span> $ 10.00</p>
+                <div className="prodDetails__buy">
+                    <p><span className="prodDetails__property prodDetails__price ">Price:</span> $ {price}</p>
                     <ProductButton text="Buy Now" style="btn btn-secondary"/>
                 </div>
             </div>
-         </div>
      </section>
     )
 };
