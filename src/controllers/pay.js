37e7dd11cb2225334d95ebb15402986e4aed8771
Diego Cruz <dacruzt@outlook.com>
2021-06-03 10:57:48 -0500

    complete payment process!

--- src/controllers/pay.js
+++ src/controllers/pay.js
@@ -8,7 +8,7 @@ const db = mysql.createPool({
     database: process.env.DATABASE
 });
 
-exports.process = async (req, res) => {
+exports.process = (req, resul) => {
     const {
         name,
         type,
@@ -19,34 +19,50 @@ exports.process = async (req, res) => {
         id_p
     } = req.body;
 
-    db.query('INSERT INTO payments SET ?', {
-        name_pa: name,
-        type_pa: type,
-        quantity: quantity,
-        total: total        
-    }, (error, results) => {
-        if (error) {
-            console.log(error);
-        } else {
-            const id_payment = results.insertId;
-            db.query('INSERT INTO buys SET ?', {
-                user_seller: id_s,
-                user_buyer: id_b,
-                product_b_fk: id_p,
-                payment_fk: id_payment
+    db.query(`SELECT stock FROM  products WHERE id_p=${id_p}`, (err, res) => {
+        if (res[0].stock > 0) {
+            db.query('INSERT INTO payments SET ?', {
+                name_pa: name,
+                type_pa: type,
+                quantity: quantity,
+                total: total
             }, (error, results) => {
                 if (error) {
                     console.log(error);
                 } else {
-                    console.log("dato insertado tabla buys");
+                    const id_payment = results.insertId;
+                    db.query('INSERT INTO buys SET ?', {
+                        user_seller: id_s,
+                        user_buyer: id_b,
+                        product_b_fk: id_p,
+                        payment_fk: id_payment
+                    }, (error, results) => {
+                        if (error) {
+                            console.log(error);
+                        } else {
+                            console.log("dato insertado tabla buys");
+                            db.query(`UPDATE products SET stock=stock - ${quantity} WHERE id_p=${id_p}`, (error, results) => {
+                                console.log("stock actualizado")
+                                return resul.json({
+                                    "status": 200,
+                                    "message:": 'Payment registered',
+                                });
+
+                            });
+
+                        }
+
+                    });
+
+                }
+
+            });
+        } else {
+            return resul.json({
+                "error": {
+                    "message": 'No stock available'
                 }
             });
-            return res.json({
-                "status": 200,
-                "message:": 'Payment registered',
-            });
         }
-
     });
-
 }
